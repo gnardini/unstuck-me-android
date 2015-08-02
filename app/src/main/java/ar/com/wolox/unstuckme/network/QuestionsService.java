03cb3cb5a17a1807e478132f5fbf5d9050e73c13
Juan Ignacio <juanignaciomln@gmail.com>
2015-08-02 01:55:03 -0300

    Vote image backend

--- app/src/main/java/ar/com/wolox/unstuckme/network/QuestionsService.java
+++ app/src/main/java/ar/com/wolox/unstuckme/network/QuestionsService.java
@@ -2,27 +2,32 @@ package ar.com.wolox.unstuckme.network;
 
 import java.util.List;
 
+import ar.com.wolox.unstuckme.Configuration;
 import ar.com.wolox.unstuckme.model.Question;
+import ar.com.wolox.unstuckme.model.VotesBatch;
 import retrofit.Callback;
+import retrofit.http.Body;
 import retrofit.http.GET;
-import retrofit.http.Path;
+import retrofit.http.POST;
 import retrofit.http.Query;
 
 public interface QuestionsService {
 
-    @GET("/api/v1/questions")
-    void getQuestions(@Query("page") int page,
-                        Callback<List<Question>> db);
+    @GET(Configuration.API_PREFIX + "questions")
+    void getQuestions(Callback<List<Question>> db);
 
-    @GET("/api/v1/questions/my_answers")
+    @GET(Configuration.API_PREFIX + "questions/my_answers")
     void getMyAnswers(
             @Query("page") int page,
             @Query("amount") int amountPerPage,
             Callback<List<Question>> db);
 
-    @GET("/api/v1/questions/my_questions")
+    @GET(Configuration.API_PREFIX + "questions/my_questions")
     void getMyQuestions(
             @Query("page") int page,
             @Query("amount") int amountPerPage,
             Callback<List<Question>> db);
+
+    @POST(Configuration.API_PREFIX + "questions/vote")
+    void sendVotes(@Body VotesBatch votesBatch, Callback<List<Question>> db);
 }
