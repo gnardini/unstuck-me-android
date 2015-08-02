8b25701e07055d573bc66a723ef7e5a398ad6079
gnardini <gonzalo.nardini@wolox.com.ar>
2015-08-02 06:28:48 -0300

    Added levelup push

--- app/src/main/java/ar/com/wolox/unstuckme/network/notification/PushReceiver.java
+++ app/src/main/java/ar/com/wolox/unstuckme/network/notification/PushReceiver.java
@@ -18,7 +18,9 @@ public class PushReceiver extends ParsePushBroadcastReceiver {
     private static final int APP_PUSH_ID = 17523;
 
     public static final String QUESTION_ID = "question_id";
-
+    public static final String TYPE = "type";
+    public static final String LIMIT = "limit";
+    public static final String LEVELUP = "levelup";
 
     private static final String PARSE_DATA_PARAMETER = "com.parse.Data";
     private static final String DATA = "data";
@@ -32,16 +34,25 @@ public class PushReceiver extends ParsePushBroadcastReceiver {
         ParseAnalytics.trackAppOpenedInBackground(intent);
         mNotificationsCount = 0;
         int questionId;
+        String type;
         try {
             JSONObject data = new JSONObject(intent.getStringExtra(PARSE_DATA_PARAMETER));
             questionId = data.getInt(QUESTION_ID);
+            type = data.getString(DATA);
         } catch (JSONException e) {
             questionId = Configuration.QUESTION_ID_ERROR;
+            type = null;
         }
-        Intent newIntent = new Intent(context, MainActivity.class);
-        newIntent.putExtra(QUESTION_ID, questionId);
-        newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(newIntent);
+        if (type != null) {
+            if (type.equals(LIMIT)) {
+                Intent newIntent = new Intent(context, MainActivity.class);
+                newIntent.putExtra(QUESTION_ID, questionId);
+                newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(newIntent);
+            } else if (type.equals(LEVELUP)) {
+                // TODO
+            }
+        }
     }
 
     @Override
@@ -52,7 +63,11 @@ public class PushReceiver extends ParsePushBroadcastReceiver {
             int questionId = pushData
                     .getJSONObject(DATA)
                     .getInt(QUESTION_ID);
+            String type = pushData
+                    .getJSONObject(DATA)
+                    .getString(TYPE);
             pushData.put(QUESTION_ID, questionId);
+            pushData.put(DATA, type);
             pushData.put(ALERT, contextIntent.getString(R.string.push_notification_text));
             pushData.put(TITLE, contextIntent.getString(R.string.app_name));
             intent.putExtra(PARSE_DATA_PARAMETER, pushData.toString());
