9252cd4feb7a775cd7a43cf0025b65a3a5df07a9
Gonzalo Nardini <gnardini@itba.edu.ar>
2015-08-04 23:08:37 -0300

    Auto add of answered questions to My Answers tab

--- app/src/main/java/ar/com/wolox/unstuckme/fragment/results/MyAnswersFragment.java
+++ app/src/main/java/ar/com/wolox/unstuckme/fragment/results/MyAnswersFragment.java
@@ -1,10 +1,14 @@
 package ar.com.wolox.unstuckme.fragment.results;
 
+import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import ar.com.wolox.unstuckme.R;
+import ar.com.wolox.unstuckme.model.event.QuestionAnsweredEvent;
 import ar.com.wolox.unstuckme.network.provider.MyAnswersProvider;
 import ar.com.wolox.unstuckme.network.provider.Provider;
+import de.greenrobot.event.EventBus;
 
 public class MyAnswersFragment extends ResultsFragment {
 
@@ -22,5 +26,24 @@ public class MyAnswersFragment extends ResultsFragment {
     @Override
     protected Provider loadProvider() {
         return new MyAnswersProvider();
+    }
+
+    public void onEvent(QuestionAnsweredEvent event) {
+        mList.add(0, event.getQuestion());
+        if (mList.size() > ELEMENTS_PER_PAGE) mList.remove(mList.size() - 1);
+        mAdapter.notifyDataSetChanged();
+        mNoResultsView.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
     }
 }
